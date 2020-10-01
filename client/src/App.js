c02de2df3c5df3840cf7d23b00d02218ed8c1c1f
koyeary <katyeary@gmail.com>
2020-09-30 22:31:11 -0700

    big commit, backtracking using the react library app from activities to fix front end error

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./components/Books";
-import Detail from "./components/Detail";
-import NoMatch from "./components/NoMatch";
-import Nav from "./sections/Nav";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
 function App() {
   return (
--- models/book.js
+++ models/book.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  authors: { type: String, required: true },
-  link: { type: String, required: true},
-  synopsis: String,
+  authors: { type: [String], required: true },
+  link: { type: String, required: true },
+  description: { String, required: true },
   image: { type: String, required: true },
   googleId: { type: String, required: true, unique: true }
 });
