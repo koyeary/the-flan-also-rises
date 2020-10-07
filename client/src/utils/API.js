7123624bb97b956fdbd618ba24e8ee8d25b2cd55
koyeary <katyeary@gmail.com>
2020-10-07 11:19:24 -0700

    increase search threshold

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
   getBook: function (query) {
-    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=10`);
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=40`);
   },
   // Deletes the book with the given id
   deleteBook: function (id) {
